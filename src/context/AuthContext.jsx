8c2981381daa325000cd06d7dcbe8c0c561d010b
itzme-challa <challawaheed4@gmail.com>
2025-05-19 22:39:52 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react'; // Updated import
 import { auth } from '../firebase/config';
 import { onAuthStateChanged } from 'firebase/auth';
 
