37f88158f8251ae00479cb443b097ab188be8c72
itzme-challa <challawaheed4@gmail.com>
2025-05-19 22:33:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Header from './components/Header';
@@ -8,22 +9,37 @@ import EditPage from './pages/EditPage';
 import UserPage from './pages/UserPage';
 import './App.css';
 
+class ErrorBoundary extends React.Component {
+  state = { error: null };
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+  render() {
+    if (this.state.error) {
+      return <div className="error">Something went wrong: {this.state.error.message}</div>;
+    }
+    return this.props.children;
+  }
+}
+
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <div className="app">
-          <Header />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/article/:title" element={<ArticlePage />} />
-              <Route path="/create" element={<CreatePage />} />
-              <Route path="/edit/:title" element={<EditPage />} />
-              <Route path="/user/:userId" element={<UserPage />} />
-            </Routes>
+        <ErrorBoundary>
+          <div className="app">
+            <Header />
+            <div className="content">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/article/:title" element={<ArticlePage />} />
+                <Route path="/create" element={<CreatePage />} />
+                <Route path="/edit/:title" element={<EditPage />} />
+                <Route path="/user/:userId" element={<UserPage />} />
+              </Routes>
+            </div>
           </div>
-        </div>
+        </ErrorBoundary>
       </AuthProvider>
     </Router>
   );
