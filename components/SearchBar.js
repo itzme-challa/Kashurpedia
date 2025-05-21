4b8b84e888480f45f792fdadd6ec9ec8d9d83218
itzme-challa <challawaheed4@gmail.com>
2025-05-21 09:49:37 +0530

    Update SearchBar.js

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,10 +1,20 @@
 export default function SearchBar({ query, setQuery }) {
   return (
-    <input
-      type="text"
-      placeholder="Search articles..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-    />
+    <div style={{ margin: '20px 0' }}>
+      <input
+        type="search"
+        placeholder="Search Kashurpedia..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        style={{
+          width: '100%',
+          maxWidth: '500px',
+          padding: '10px',
+          fontSize: '16px',
+          border: '1px solid #a2a9b1',
+          borderRadius: '2px'
+        }}
+      />
+    </div>
   );
 }
