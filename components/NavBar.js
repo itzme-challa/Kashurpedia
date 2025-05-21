768038b7dab6c9f404366a6317b3803904a75398
itzme-challa <challawaheed4@gmail.com>
2025-05-21 10:35:29 +0530

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,41 +1,54 @@
 import Link from "next/link";
+import { useState } from "react";
 import { auth } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
 
 export default function NavBar() {
   const [user] = useAuthState(auth);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className="navbar">
-      <div className="nav-links">
-        <Link href="/" className="nav-link" data-icon="home" data-tooltip="Home">
-          <span className="nav-link-text">Kashurpedia</span>
-        </Link>
-        <Link href="/submit" className="nav-link" data-icon="add" data-tooltip="Submit Article">
-          <span className="nav-link-text">Submit</span>
+      <div style={{ display: "flex", alignItems: "center", gap: "1em" }}>
+        <button 
+          className="mobile-menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <Link href="/">
+          <span className="nav-text">Kashurpedia</span>
         </Link>
       </div>
       
-      <div className="nav-links">
+      <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <Link href="/submit">
+          <span className="nav-text">Submit Article</span>
+        </Link>
+        
         {user ? (
           <>
-            <span className="welcome-message">Welcome, {user.displayName || user.email}</span>
+            <span style={{ padding: "1em" }}>Welcome, {user.displayName || user.email}</span>
             <button 
               onClick={() => signOut(auth)}
-              className="nav-button"
-              data-tooltip="Log out"
+              style={{ 
+                background: "none", 
+                border: "none", 
+                color: "var(--link-color)",
+                padding: "1em",
+                textAlign: "left"
+              }}
             >
-              <span>Log out</span>
+              <i className="fas fa-sign-out-alt" style={{ marginRight: "0.5em" }}></i>
+              <span className="nav-text">Log out</span>
             </button>
           </>
         ) : (
           <>
-            <Link href="/auth/login" className="nav-link" data-icon="login" data-tooltip="Log in">
-              <span className="nav-link-text">Log in</span>
+            <Link href="/auth/login">
+              <span className="nav-text">Log in</span>
             </Link>
-            <Link href="/auth/signup" className="nav-link" data-icon="signup" data-tooltip="Create account">
-              <span className="nav-link-text">Create account</span>
+            <Link href="/auth/signup">
+              <span className="nav-text">Create account</span>
             </Link>
           </>
         )}
