a4e7c8f847d2e63b1d2fc06671de9ba5163ba548
itzme-challa <challawaheed4@gmail.com>
2025-05-21 09:37:02 +0530

    Update signup.js

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,29 +1,27 @@
 import { useState } from "react";
 import { register } from "../../utils/auth";
 import { useRouter } from "next/router";
+import NavBar from "../../components/NavBar";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+  const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const router = useRouter();
 
   const handleSignup = async () => {
-    try {
-      await register(email, pass, username);
-      router.push("/");
-    } catch (err) {
-      alert(err.message);
-    }
+    await register(email, password, username);
+    router.push("/");
   };
 
   return (
     <div>
+      <NavBar />
       <h1>Signup</h1>
       <input placeholder="Username" onChange={e => setUsername(e.target.value)} />
       <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={e => setPass(e.target.value)} />
-      <button onClick={handleSignup}>Signup</button>
+      <input placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleSignup}>Register</button>
     </div>
   );
 }
