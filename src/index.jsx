ffd0733243a3bab579a0d5910f48b1bc849ee615
itzme-challa <challawaheed4@gmail.com>
2025-05-21 09:23:37 +0530

    Delete src directory

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './App.css';
-
-console.log('index.jsx: Starting');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-console.log('index.jsx: Rendered');
