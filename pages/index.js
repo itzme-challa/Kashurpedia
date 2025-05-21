9a49c9204eedf8e05582e05e1381c0e3ce471175
itzme-challa <challawaheed4@gmail.com>
2025-05-21 09:48:40 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { db } from "../utils/firebase";
 import { ref, onValue } from "firebase/database";
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import SearchBar from "../components/SearchBar";
 
 export default function Home() {
@@ -28,22 +28,32 @@ export default function Home() {
   }, {});
 
   return (
-    <div>
-      <NavBar />
-      <h1>Kashurpedia</h1>
-      <SearchBar query={query} setQuery={setQuery} />
-      {Object.keys(filteredArticles).map((category) => (
-        <div key={category}>
-          <h2>{category}</h2>
-          <ul>
-            {Object.entries(filteredArticles[category]).map(([id, article]) => (
-              <li key={id}>
-                <Link href={`/article/${id}`}>{article.title}</Link>
-              </li>
+    <Layout>
+      <div className="wiki-content">
+        <h1>Welcome to Kashurpedia</h1>
+        <p>The free encyclopedia that anyone can edit.</p>
+        
+        <SearchBar query={query} setQuery={setQuery} />
+        
+        {Object.keys(filteredArticles).length > 0 ? (
+          <div className="category-list">
+            {Object.keys(filteredArticles).map((category) => (
+              <div key={category} style={{ breakInside: 'avoid', marginBottom: '20px' }}>
+                <h2>{category}</h2>
+                <ul className="article-list">
+                  {Object.entries(filteredArticles[category]).map(([id, article]) => (
+                    <li key={id}>
+                      <Link href={`/article/${id}`}>{article.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             ))}
-          </ul>
-        </div>
-      ))}
-    </div>
+          </div>
+        ) : (
+          <p>No articles found. {query && 'Try a different search term.'}</p>
+        )}
+      </div>
+    </Layout>
   );
 }
