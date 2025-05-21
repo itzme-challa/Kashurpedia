cf7a06da9e7d36af8e74fd2c7278404001bed794
itzme-challa <challawaheed4@gmail.com>
2025-05-21 09:35:20 +0530

    Update [id].js

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,18 +1,27 @@
 import { useRouter } from "next/router";
-import { db, get, child, ref } from "../../utils/firebase";
 import { useEffect, useState } from "react";
+import { db, auth } from "../../utils/firebase";
+import { ref, get, child, remove } from "firebase/database";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Link from "next/link";
+import NavBar from "../../components/NavBar";
+import Head from "next/head";
 
 export default function ArticlePage() {
-  const { id } = useRouter().query;
+  const router = useRouter();
+  const { id } = router.query;
   const [article, setArticle] = useState(null);
+  const [user] = useAuthState(auth);
 
   useEffect(() => {
     if (!id) return;
-    get(child(ref(db), "articles")).then(snapshot => {
+    const articlesRef = ref(db, "articles");
+    get(articlesRef).then((snapshot) => {
       if (snapshot.exists()) {
-        for (let cat in snapshot.val()) {
-          if (snapshot.val()[cat][id]) {
-            setArticle(snapshot.val()[cat][id]);
+        const data = snapshot.val();
+        for (let category in data) {
+          if (data[category][id]) {
+            setArticle({ ...data[category][id], category });
             break;
           }
         }
@@ -20,14 +29,18 @@ export default function ArticlePage() {
     });
   }, [id]);
 
+  const handleDelete = async () => {
+    if (confirm("Are you sure you want to delete this article?")) {
+      await remove(ref(db, `articles/${article.category}/${id}`));
+      router.push("/");
+    }
+  };
+
   if (!article) return <p>Loading...</p>;
 
   return (
     <div>
-      <h1>{article.title}</h1>
-      <p><i>Category: {article.category}</i></p>
-      <hr />
-      <div dangerouslySetInnerHTML={{ __html: article.content }} />
-    </div>
-  );
-}
+      <NavBar />
+      <Head>
+        <title>{article.title} - Kashurpedia</title>
+       0
